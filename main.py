4deaf733d6a2c014089df6026a5bb055f4c0c73e
Downyk <TeamBehindDowny@gmail.com>
2022-12-17 01:52:21 +0000

    add ideed crawling

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ from extractors.wwr import extract_wwr_jobs
 
 jobs = extract_wwr_jobs("python")
 print(jobs)
+
+base_url = "https://kr.indeed.com/jobs?q="
+search_term = "python"
+
+response = get(f"{base_url}{search_term}")
+
+if response.status_code != 200:
+  print('Cant request page')
+else:
+  #print(response.text)
+  soup = BeautifulSoup(response.text, "html.parser")
+  job_list = soup.find("ul", class_="jobsearch_ResultsList")
+  jobs = job_list.find_all('li')
+  print(len(jobs))
