3cb1c42d5ae520f1d28368e6e70718daa64690e6
Downyk <TeamBehindDowny@gmail.com>
2022-12-18 16:09:17 +0000

    add excel maker

--- extractors/wwr.py
+++ extractors/wwr.py
@@ -31,9 +31,9 @@ def extract_wwr_jobs(keyword):
         #print('/////////////////////////////')
         job_data = {
           'link': f'https://weworkremotely.com{link}',
-          'company': company.string,
-          'region': region.string,
-          'position': title.string
+          'company': company.string.replace(',', ' '),
+          'location': region.string.replace(',', ' '),
+          'position': title.string.replace(',', ' ')
         }
         results.append(job_data)
     #print(results)
--- main.py
+++ main.py
@@ -5,9 +5,13 @@ keyword = input('what do you want to search for?')
 
 indeed = extract_indeed_jobs(keyword)
 wwr = extract_wwr_jobs(keyword)
-
 jobs = indeed + wwr
 
+file = open(f"{keyword}.csv", "w")
+file.write("Position, Company, Location, URL\n")
+
 for job in jobs:
-  print(job)
-  print("/////")
+  file.write(
+    f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
+
+file.close()
