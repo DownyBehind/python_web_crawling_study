340f699ed9404641e9419016a900dbd80efe32f3
Downyk <TeamBehindDowny@gmail.com>
2022-12-18 13:15:56 +0000

    update indeed page

--- main.py
+++ main.py
@@ -1,20 +1,64 @@
 from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-jobs = extract_wwr_jobs("python")
-print(jobs)
 
-base_url = "https://kr.indeed.com/jobs?q="
-search_term = "python"
+def get_page_count(keyword):
+  # this options is the thing for using chrome in replit
+  options = Options()
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
 
-response = get(f"{base_url}{search_term}")
+  browser = webdriver.Chrome(options=options)
+  browser.get(f"https:/kr.indeed.com/jobs?q={keyword}")
+  soup = BeautifulSoup(browser.page_source, "html.parser")
+  pagination = soup.find('nav', class_='ecydgvn0')
+  if pagination == None:
+    # only page is one
+    return 1
+  else:
+    pages = pagination.find_all('div', recursive=False)
+    count = len(pages)
+    if count >= 5:
+      return 5
+    else:
+      return count
 
-if response.status_code != 200:
-  print('Cant request page')
-else:
-  #print(response.text)
-  soup = BeautifulSoup(response.text, "html.parser")
-  job_list = soup.find("ul", class_="jobsearch_ResultsList")
-  jobs = job_list.find_all('li')
-  print(len(jobs))
+
+def extract_indeed_jobs(keyword):
+  pages = get_page_count(keyword)
+  for page in range(pages):
+
+    base_url = "https://kr.indeed.com/jobs"
+    # this options is the thing for using chrome in replit
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    browser = webdriver.Chrome(options=options)
+    browser.get(f"{base_url}?q={keyword}&start={page*10}")
+    results = []
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    job_list = soup.find("ul", class_="jobsearch-ResultsList")
+    jobs = job_list.find_all('li', recursive=False)
+    for job in jobs:
+      zone = job.find('div', class_='mosaic-zone')
+      if zone == None:
+        anchor = job.select_one('h2 a')
+        title = anchor['aria-label']
+        link = anchor['href']
+        company = job.find('span', class_='companyName')
+        location = job.find('div', class_='companyLocation')
+        job.data = {
+          'link': f'https://kr.indeed.com{link}',
+          'company': company.string,
+          'location': location.string,
+          'position': title
+        }
+        results.append(job.data)
+
+    for result in results:
+      print(result)
+      print('//////')
