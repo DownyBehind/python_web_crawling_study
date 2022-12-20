e9d2fe143608c906f3ec2ecea5052ff31ffb29e6
Downyk <TeamBehindDowny@gmail.com>
2022-12-20 14:07:28 +0000

    add flask app

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
+from flask import Flask
 
+app = Flask("JobScrapper")
+app.run("0.0.0.0")
+
+
+
+'''
 keyword = input('what do you want to search for?')
 
 indeed = extract_indeed_jobs(keyword)
 wwr = extract_wwr_jobs(keyword)
 jobs = indeed + wwr
 
-file = open(f"{keyword}.csv", "w")
-file.write("Position, Company, Location, URL\n")
-
-for job in jobs:
-  file.write(
-    f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
-
-file.close()
+save_to_file(keyword, jobs)
+'''
